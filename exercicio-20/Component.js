b3b05a95dcd5b0babd0b0ac8c6ba1776e2486685
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-12-05 21:48:05 -0300

    exercicio-20 classes para o DOM parte2

--- exercicio-20/Component.js
+++ exercicio-20/Component.js
@@ -1,30 +1,28 @@
 export class Component {
-    #element = null;
-
-    constructor(tag, parent, options){
-        this.tag = tag;
-        this.parent = parent;
-        this.options = options;
-        this.build();
+    #element = null
+  
+    constructor(tag, parent, options) {
+      this.tag = tag
+      this.parent = parent
+      this.options = options
+      this.build()
     }
-
-    get element(){
-        return this.#element;
+  
+    getElement() {
+      return this.#element
     }
-
-    build(){
-        this.#element = document.createElement(this.tag);
-        Object.assign(this.#element, this.options);
-        return this
+  
+    build() {
+      this.#element = document.createElement(this.tag)
+      Object.assign(this.#element, this.options)
+      return this
     }
-
-    render(){
-        if (this.parent instanceof Component){
-            this.parent.#element.append(this.#element);
-        } else{
-            document.querySelector(this.parent).append(this.#element);
-        }
+  
+    render() {
+      if (this.parent instanceof Component) {
+        this.parent.getElement().append(this.#element)
+      } else {
+        document.querySelector(this.parent).append(this.#element)
+      }
     }
-}
-
-
+  }
